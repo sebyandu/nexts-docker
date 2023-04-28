9894ea1b8120d350a616ddbc7e086acf20b734ae
SebastiAndrei <podaru.sebastian@gmail.com>
2023-04-28 14:32:23 +0300

    - test v1 iframe in layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,126 +1,225 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import variables from "./variables.module.scss";
-
-const inter = Inter({ subsets: ["latin"] });
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by
-          <span style={{ color: variables.primaryColor }}> editing</span>&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <div className='flex min-h-screen flex-col items-end justify-end bg-indigo-50'>
+      {/* <!-- chat box --> */}
+
+      <div className='flex h-96 w-80 flex-col border bg-white shadow-md'>
+        <div className='flex items-center justify-between border-b p-2'>
+          {/* <!-- user info --> */}
+          <div className='flex items-center'>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              className='h-10 w-10 rounded-full'
+              width={256}
+              height={256}
+              src='https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+              alt={''}
             />
-          </a>
+            <div className='pl-2'>
+              <div className='font-semibold'>
+                <a className='hover:underline' href='#'>
+                  John Doe
+                </a>
+              </div>
+              <div className='text-xs text-gray-600'>Online</div>
+            </div>
+          </div>
+          {/* <!-- end user info --> */}
+          {/* <!-- chat box action --> */}
+          <div>
+            <a
+              className='inline-flex rounded-full p-2 hover:bg-indigo-50'
+              href='#'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-6 w-6'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5l-5-5m5 5v-4m0 4h-4'
+                />
+              </svg>
+            </a>
+
+            <button
+              className='inline-flex rounded-full p-2 hover:bg-indigo-50'
+              type='button'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-6 w-6'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M6 18L18 6M6 6l12 12'
+                />
+              </svg>
+            </button>
+          </div>
+          {/* <!-- end chat box action --> */}
+        </div>
+
+        <div className='flex-1 overflow-y-auto px-4 py-4'>
+          {/* <!-- chat message --> */}
+
+          <div className='mb-4 flex items-center'>
+            <div className='mr-4 flex flex-none flex-col items-center space-y-1'>
+              <Image
+                className='h-10 w-10 rounded-full'
+                src='https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                width={256}
+                height={256}
+                alt={''}
+              />
+              <a href='#' className='block text-xs hover:underline'>
+                John Doe
+              </a>
+            </div>
+            <div className='relative mb-2 flex-1 rounded-lg bg-indigo-400 p-2 text-white'>
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </div>
+
+              {/* <!-- arrow --> */}
+              <div className='absolute left-0 top-1/2 h-2 w-2 -translate-x-1/2 rotate-45 transform bg-indigo-400'></div>
+              {/* <!-- end arrow --> */}
+            </div>
+          </div>
+
+          {/* <!-- end chat message --> */}
+
+          {/* <!-- chat message --> */}
+
+          <div className='mb-4 flex flex-row-reverse items-center'>
+            <div className='ml-4 flex flex-none flex-col items-center space-y-1'>
+              <Image
+                className='h-10 w-10 rounded-full'
+                width={256}
+                height={256}
+                src='https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                alt={''}
+              />
+              <a href='#' className='block text-xs hover:underline'>
+                Jesse
+              </a>
+            </div>
+            <div className='relative mb-2 flex-1 rounded-lg bg-indigo-100 p-2 text-gray-800'>
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem
+                ipsum dolor sit amet, consectetur adipisicing elit.
+              </div>
+
+              {/* <!-- arrow --> */}
+              <div className='absolute right-0 top-1/2 h-2 w-2 translate-x-1/2 rotate-45 transform bg-indigo-100'></div>
+              {/* <!-- end arrow --> */}
+            </div>
+          </div>
+
+          {/* <!-- end chat message --> */}
+
+          {/* <!-- chat message --> */}
+
+          <div className='mb-4 flex items-center'>
+            <div className='mr-4 flex flex-none flex-col items-center space-y-1'>
+              <Image
+                className='h-10 w-10 rounded-full'
+                alt='chat'
+                width={256}
+                height={256}
+                src='https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+              />
+              <a href='#' className='block text-xs hover:underline'>
+                John Doe
+              </a>
+            </div>
+            <div className='relative mb-2 flex-1 rounded-lg bg-indigo-400 p-2 text-white'>
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </div>
+
+              {/* <!-- arrow --> */}
+              <div className='absolute left-0 top-1/2 h-2 w-2 -translate-x-1/2 rotate-45 transform bg-indigo-400'></div>
+              {/* <!-- end arrow --> */}
+            </div>
+          </div>
+
+          {/* <!-- end chat message --> */}
+        </div>
+
+        <div className='flex items-center border-t p-2'>
+          {/* <!-- chat input action --> */}
+          <div>
+            <button
+              className='inline-flex rounded-full p-2 hover:bg-indigo-50'
+              type='button'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-6 w-6'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M12 6v6m0 0v6m0-6h6m-6 0H6'
+                />
+              </svg>
+            </button>
+          </div>
+          {/* <!-- end chat input action --> */}
+
+          <div className='mx-2 w-full'>
+            <input
+              className='w-full rounded-full border border-gray-200'
+              type='text'
+              placeholder='Aa'
+            />
+          </div>
+
+          {/* <!-- chat send action --> */}
+
+          <div>
+            <button
+              className='inline-flex rounded-full p-2 hover:bg-indigo-50'
+              type='button'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-6 w-6'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z'
+                />
+              </svg>
+            </button>
+          </div>
+
+          {/* <!-- end chat send action --> */}
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      {/* <!-- end chat box --> */}
+    </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    domains: ['images.unsplash.com'],
+  },
 };
 
 module.exports = nextConfig;
